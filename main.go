1c88d75a374b291e80158c66aa2e41a4f97c5807
Chris <rchristian.ramon@gmail.com>
2014-11-27 19:18:49 -0500

    first commit, charging cc works :)

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	stripe "github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/charge"
+	"github.com/stripe/stripe-go/currency"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	AmountToCharge uint64 = 10000
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func createDebit(token string, amount uint64, description string) *stripe.Charge {
+	stripe.Key = os.Getenv("STRIPE_KEY")
+
+	params := &stripe.ChargeParams{
+		Amount:   amount,
+		Currency: currency.USD,
+		Card: &stripe.CardParams{
+			Token: token,
+		},
+		Desc: description,
+	}
+
+	ch, err := charge.New(params)
+
+	if err != nil {
+		log.Fatalf("error while trying to charge a cc", err)
+	}
+
+	log.Printf("debit created successfully %v\n", ch.ID)
+
+	return ch
+}
+
+func debitsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		createDebit(r.FormValue("stripeToken"), AmountToCharge, "testing charge description!")
+		fmt.Fprint(w, "successful payment.")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/debits", debitsHandler)
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8081", nil)
+}
---
+++ static/stripe.formHandler.js
@@ -0,0 +1,30 @@
+function stripeResponseHandler(status, response) {
+  var $form = $('#payment-form');
+
+  if (response.error) {
+    // Show the errors on the form
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('button').prop('disabled', false);
+  } else {
+    // response contains id and card, which contains additional card details
+    var token = response.id;
+    // Insert the token into the form so it gets submitted to the server
+    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+    // and submit
+    $form.get(0).submit();
+  }
+};
+
+$(function() {
+  $('#payment-form').submit(function(event) {
+    var $form = $(this);
+
+    // Disable the submit button to prevent repeated clicks
+    $form.find('button').prop('disabled', true);
+
+    Stripe.card.createToken($form, stripeResponseHandler);
+
+    // Prevent the form from submitting with the default action
+    return false;
+  });
+});
