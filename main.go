bfad5739f04f6834b1311c86c6c57827d0c30ca3
Chris <rchristian.ramon@gmail.com>
2014-11-27 19:40:42 -0500

    now using env var for stripe publishable key.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	stripe "github.com/stripe/stripe-go"
 	"github.com/stripe/stripe-go/charge"
 	"github.com/stripe/stripe-go/currency"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -14,8 +15,16 @@ const (
 	AmountToCharge uint64 = 10000
 )
 
+type Home struct {
+	PublishableKey string
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+	t, _ := template.ParseFiles("home.html")
+	home := Home{
+		PublishableKey: os.Getenv("STRIPE_PUBLISHABLE_KEY"),
+	}
+	t.Execute(w, home)
 }
 
 func createDebit(token string, amount uint64, description string) *stripe.Charge {
